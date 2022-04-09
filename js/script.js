4a0ddb72f2ac2b06ef8eb123c6246c6cc2b1c72d
Ada Jasik <jolly.zombie@protonmail.com>
2022-04-09 21:08:29 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 'use strict';
 
 const titleClickHandler = function (event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
+  for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.active');
 
-  for(let activeArticle of activeArticles){
+  for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
