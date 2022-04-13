cd74952bf6ceb34d98c7b6cbc9daf153718adbb1
Ada Jasik <jolly.zombie@protonmail.com>
2022-04-13 10:37:59 +0200

    ESLint configuration

--- js/script.js
+++ js/script.js
@@ -33,24 +33,25 @@
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
     console.log('target article-active: ', targetArticle);
-  }
+  };
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-  function generateTitleLinks() {
+
+  // eslint-disable-next-line no-inner-declarations
+  function generateTitleLinks (){
 
     /* [DONE] remove the contents of titlelist */
     const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = '';
 
     /* [DONE] find all the articles and save them to variable: articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
     let html = '';
 
-    for (let article of articles) {
+    for (let article of articles){
 
       /* [DONE] get the article id */
       const articleId = article.getAttribute('id');
@@ -59,7 +60,7 @@
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
       /* [DONE] create HTML of the link */
-      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle +'</span></a></li>';
       console.log(linkHTML);
 
       /* [DONE] insert link into titleList */
@@ -72,7 +73,7 @@
     const links = document.querySelectorAll('.titles a');
 
     for (let link of links) {
-      link.addEventListener('click', titleClickHandler)
+      link.addEventListener('click', titleClickHandler);
     }
   }
   generateTitleLinks();
