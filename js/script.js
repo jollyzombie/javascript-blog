63888bb956df1084f82280819257f36aa57559be
Ada Jasik <jolly.zombie@protonmail.com>
2022-04-19 12:27:19 +0200

    Add display a list of tags

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
   const optTitleListSelector = '.titles';
   const optArticleTagsSelector = '.post-tags, .list';
   const optArticleAuthorSelector = '.post-author';
+  const optTagsListSelector = '.list.tags';
 
 
   const titleClickHandler = function (event) {
@@ -57,21 +58,51 @@
 
 
   function generateTags() {
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
+    console.log(allTags);
+
+    /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
+    /* START LOOP: for every article: */
     for (let article of articles) {
+      /* find tags wrapper */
       const tagList = article.querySelector(optArticleTagsSelector);
+
+      /* make html variable with empty string */
       let html = '';
+
+      /* get tags from data-tags attribute */
       const articleTags = article.getAttribute('data-tags');
+
+      /* split tags into array */
       const articleTagsArray = articleTags.split(' ');
 
-      for (let tag of articleTagsArray) {
-        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-        html = html + linkHTML;
-      }
 
+      /* START LOOP: for each tag */
+      for (let tag of articleTagsArray) {
+
+        /* generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+
+        /* add generated code to html variable */
+        html = html + linkHTML;
+
+        /* [NEW] check if this link is NOT already in allTags */
+        if (allTags.indexOf(linkHTML) == -1) {
+
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
+      }
       tagList.innerHTML = html;
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] add HTML from allTags to taglist */
+    tagList.innerHTML = allTags.join(' ');
   }
 
 
